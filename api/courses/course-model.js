9873648cd89ecf4efc4442c8d022b5d7e4e46703
ngwatso <ngwatson79@yahoo.com>
2021-04-25 10:26:06 -0400

    rollback commit, removing table user_class

---
+++ api/courses/course-model.js
@@ -0,0 +1,22 @@
+const db = require('../../data/db-config');
+
+// TODO findAll ==> GET
+
+// TODO findById(id) ==> GET
+
+// TODO findBy(filter) ==> GET
+
+// TODO createCourse ==> POST
+
+// TODO updateCourse ==> PUT
+
+// TODO deleteCourse ==> DELETE
+
+module.exports = {
+	findAll,
+	findById,
+	findBy,
+	createCourse,
+	updateCourse,
+	deleteCourse,
+};
