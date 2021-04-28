169885cb2e0d3190657784eb4f60be2432b3ba31
ngwatso <ngwatson79@yahoo.com>
2021-04-28 10:50:09 -0400

    fixed bug in course creation

--- api/courses/course-middleware.js
+++ api/courses/course-middleware.js
@@ -3,37 +3,37 @@ const Courses = require('./course-model');
 const checkCoursePayload = async (req, res, next) => {
 	const payload = req.body;
 
-	if (!payload.course_name) {
+	if (!payload.class_name) {
 		next({
 			apiCode: 400,
 			apiMessage:
 				'Course object must contain a course name',
 		});
-	} else if (!payload.course_type) {
+	} else if (!payload.class_type) {
 		next({
 			apiCode: 400,
 			apiMessage:
 				'Course object must contain a course type',
 		});
-	} else if (!payload.course_start) {
+	} else if (!payload.class_start) {
 		next({
 			apiCode: 400,
 			apiMessage:
 				'Course object must contain a start time',
 		});
-	} else if (!payload.course_duration) {
+	} else if (!payload.class_duration) {
 		next({
 			apiCode: 400,
 			apiMessage:
 				'Course object must contain a course duration',
 		});
-	} else if (!payload.course_intensity) {
+	} else if (!payload.class_intensity) {
 		next({
 			apiCode: 400,
 			apiMessage:
 				'Course object must contain a course intensity',
 		});
-	} else if (!payload.course_max) {
+	} else if (!payload.class_max) {
 		next({
 			apiCode: 400,
 			apiMessage:
